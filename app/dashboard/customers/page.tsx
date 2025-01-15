bfb23d0d9e458908dd856b71b6b38277eb916a6c
Apfelsinchen Ritter <ushfnuk@gmail.com>
2025-01-15 19:15:22 +0100

    Removed unnecessary imports

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,4 @@
-import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
 
