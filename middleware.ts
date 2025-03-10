0c4d68cea3ad3f624f80a9d78fe74ea0e0c47ad3
Apfelsinchen Ritter <ushfnuk@gmail.com>
2025-03-10 19:21:13 +0100

    Added comments
    To middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,11 @@ import { authConfig } from "./auth.config";
 export default NextAuth(authConfig).auth;
 
 export const config = {
+  /*
+  * Match all request paths except for the ones starting with:
+  * - api (API routes)
+  * - _next/static (static files)
+  * - _next/image (image optimization files)
+  */
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
